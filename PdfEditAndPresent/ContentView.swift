4b4e33620a697401f9a33e893e935486f009c868
Brandon Ramirez <brandonramirez77@yahoo.com>
2025-11-19 13:04:56 -0800

    Initial Commit

---
+++ PdfEditAndPresent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PdfEditAndPresent
+//
+//  Created by Brandon Ramirez on 11/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PdfEditAndPresent/PdfEditAndPresentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfEditAndPresentApp.swift
+//  PdfEditAndPresent
+//
+//  Created by Brandon Ramirez on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PdfEditAndPresentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
